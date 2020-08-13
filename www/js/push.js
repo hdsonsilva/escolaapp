f166b8b34fc94ef3511f7b8bcbafc9aaa160d7df
Hudson Silva <hudson@doctum.edu.br>
2020-08-13 11:01:23 -0300

    ok

--- www/js/push.js
+++ www/js/push.js
@@ -1,18 +1,22 @@
 ///////////////// PUSH NOTIFICATION ////////////////////////////////////////
      document.addEventListener('deviceready', function () {
         //Desativar botao voltar
+        ons.notification.alert('Teste');
+        
         document.addEventListener("backbutton", function(){navigator.app.exitApp();} , false);
 
         // Enable to debug issues.
         // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
         
-        
+
 
         var dadosnotificacao = jsonData.notification.payload.additionalData ;
           if(dadosnotificacao.appaba){
             window.location.href = 'app.html?objeto='+dadosnotificacao.appaba ;
           }
    
+
+
           if(localStorage.getItem('usuarios_salvos')){
             var usuarios = localStorage.getItem('usuarios_salvos') ;
             for(i in usuarios){

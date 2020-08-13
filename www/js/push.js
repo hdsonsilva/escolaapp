6695d3e601c125951609a6cb80cc5551c9a085c6
Hudson Silva <hudson@doctum.edu.br>
2020-08-13 12:21:46 -0300

    ok

--- www/js/push.js
+++ www/js/push.js
@@ -9,19 +9,20 @@
           
           if(localStorage.getItem('usuarios_salvos')){
             ons.notification.alert('enreou');
-
+            alert('ok');
             var usuarios = localStorage.getItem('usuarios_salvos') ;
-             ons.notification.alert( localStorage.getItem('idonesignal') );
-            for(i in usuarios){
+              if(0){
+              for(i in usuarios){
 
-              ons.notification.alert( 'usu' );
-              ons.notification.alert( usuarios[i]['codigo'] +'ok');
-              window.plugins.OneSignal
-                .startInit(localStorage.getItem('idonesignal'))
-               /* .handleNotificationOpened(notificationOpenedCallback  )*/
-                .endInit();
+                ons.notification.alert( 'usu' );
+                ons.notification.alert( usuarios[i]['codigo'] +'ok');
+                window.plugins.OneSignal
+                  .startInit(localStorage.getItem('idonesignal'))
+                 /* .handleNotificationOpened(notificationOpenedCallback  )*/
+                  .endInit();
 
-              window.plugins.OneSignal.sendTag("user", localStorage.getItem('unidade')+(usuarios[i]['codigo']) );
+                window.plugins.OneSignal.sendTag("user", localStorage.getItem('unidade')+(usuarios[i]['codigo']) );
+              }
             }
           }
 

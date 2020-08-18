354eeae05c5dcc2bf3eb5d70976ef26285ffd880
Hudson Silva <hudson@doctum.edu.br>
2020-08-18 19:08:10 -0300

    ok

--- www/js/push.js
+++ www/js/push.js
@@ -8,7 +8,7 @@
    
           var notificationOpenedCallback = function(jsonData) {
               
-              ons.notification.alert(jsonData.notification.payload.additionalData.tiposolicitacao);
+              ons.notification.alert(JSON.stringify(jsonData.notification.payload.additionalData));
               /*
                //Teste de como pegar dados s no push
         //localStorage.setItem('pushInfo',jsonData.notification.payload.additionalData.sessao);

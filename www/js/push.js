c90223049a0685612ef29fcba9950b965ff30fdf
Hudson Silva <hudson@doctum.edu.br>
2020-08-18 22:54:28 -0300

    ok

--- www/js/push.js
+++ www/js/push.js
@@ -9,8 +9,19 @@
           var notificationOpenedCallback = function(jsonData) {
               
               //ons.notification.alert(jsonData.notification.payload.additionalData.dados.tiposolicitacao );
+              var abba = jsonData.notification.payload.additionalData.dados.app_tab ;
 
-               window.location.href = 'app.html?objeto='+jsonData.notification.payload.additionalData.dados.app_tab ;
+              /*Verificando se tem usuario cadastrado para logar e direcionar a aba certa*/
+              if(localStorage.getItem('usuarios_salvos')){
+                usuarios = JSON.parse(localStorage.getItem('usuarios_salvos'));
+                buscarDadosUsuario(usuarios[0]['codigo']);
+                //Loga no sistema
+                auth_check = 0 ;
+                /*Tentando logar usuario*/
+                login(localStorage.getItem('login_username'),localStorage.getItem('login_password'), abba, localStorage.getItem('login_unidade'),  localStorage.getItem('login_nome'));
+
+                window.location.href = 'app.html?objeto='+ abba ;
+              }
               /*
                //Teste de como pegar dados s no push
         //localStorage.setItem('pushInfo',jsonData.notification.payload.additionalData.sessao);

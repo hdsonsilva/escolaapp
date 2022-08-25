35731a1744871f374bb60fb94c36edb56a61e986
Hudson Silva <hudson@doctum.edu.br>
2022-08-25 15:19:09 -0300

    Descendo tudo

--- platforms/android/app/src/main/assets/www/js/view_home.js
+++ platforms/android/app/src/main/assets/www/js/view_home.js
@@ -35,19 +35,23 @@ function view_home(retornos){
     var conteudo_lista = '';
     var imagem_view = '';
     let imagembusca = '';
-    conteudo_lista += "<ons-card ><font class='font_tam1'><span class='notification notification--material baloes_blue'>&nbsp;&nbsp;now&nbsp;&nbsp;</span>  <font class='font_tam3'>Video</font> </font>";
+    let j = 0 ;
+   /* conteudo_lista += "<ons-card ><font class='font_tam1'><span class='notification notification--material baloes_blue'>&nbsp;&nbsp;now&nbsp;&nbsp;</span>  <font class='font_tam3'>Video</font> </font>";
         conteudo_lista += "<br><br>";
         conteudo_lista += "<iframe width='100%' src='https://www.youtube.com/embed/bZwxTX2pWmw?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
-        conteudo_lista += "</ons-card>";
+        conteudo_lista += "</ons-card>";*/
  if(retornos['murais']){     
     //Preenchendo a lista com cards
     for(i  in retorno){
 
         conteudo_lista += "<ons-card "+(retorno[i]['url_destino'] ? "class='clicavelhome' valor='"+(retorno[i]['url_destino'])+"'" : (retorno[i]['arquivo'] ? "class='imagemview' imagem='"+(retorno[i]['arquivo'])+"'" : " ")  )+"><font class='font_tam1'><span class='notification notification--material baloes_blue'>&nbsp;&nbsp;"+(retorno[i]['data_inicio'])+"&nbsp;&nbsp;</span>  <font class='font_tam3'>"+(retorno[i]['assunto'])+"</font> </font>";
         conteudo_lista += "<br><br>";
-        conteudo_lista += "<font class='font_text'>"+(retorno[i]['arquivo'] ? "<div id='notimg_"+i+"'><img width='100%' pos='"+i+"' src='./img/carregando.gif'></div><div id='myimg_"+i+"' ><img width='1%'  class='imagens' pos='"+i+"' src='"+(retorno[i]['arquivo']).replace("http://", "https://")+"' loading='lazy'></div><br>" : "")+quebraLinha(retorno[i]['mensagem'])+"</font>";
+        conteudo_lista += "<font class='font_text'>"+(retorno[i]['arquivo'] ? "<div id='notimg_"+i+"'><img width='100%' pos='"+i+"' src='./img/carregando.gif'></div><div id='myimg_"+i+"' ><img width='1%'  class='imagens' pos='"+i+"' src='"+(retorno[i]['arquivo']).replace("http://", "https://")+"' "+(j <= 2 ? "" : "loading='lazy' " ) +"></div><br>" : "")+quebraLinha(retorno[i]['mensagem'])+"</font>";
         conteudo_lista += "</ons-card>";
         imagembusca = retorno[i]['arquivo'] ? retorno[i]['arquivo'] : imagembusca ;
+        if(retorno[i]['arquivo']){
+            j++;
+        }
     }
 
 
